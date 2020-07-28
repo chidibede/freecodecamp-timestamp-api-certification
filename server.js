74bc926e94734b9706ea6a7a29cf6ccded33a73f
chidibede <chidibede@gmail.com>
2020-07-28 20:35:47 +0100

    Fourth deployment after update

--- server.js
+++ server.js
@@ -32,10 +32,9 @@ app.get("/api/timestamp/:date_string", function (req, res) {
     let unix_date = parseInt(dateObj.getTime().toFixed(0));
     let utc_date = dateObj.toUTCString();
     res.json({ unix: unix_date, utc: utc_date });
-  } else if (parseInt(new Date(date_string).getTime().toFixed(0)) > 0) {
+  } else if (parseInt(date_string) > 0) {
     let unix_date = parseInt(date_string);
     let dateObj = new Date(unix_date);
-    console.log(unix_date)
     let utc_date = dateObj.toUTCString();
     res.json({ unix: unix_date, utc: utc_date });
   } else {
